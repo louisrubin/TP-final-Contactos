e58d054fdeefd9f582067c616091153f5335b830
louisrubin <miguel10.rbn@gmail.com>
2022-11-11 20:45:06 -0300

    initial commit

---
+++ src/main/java/TP_final/Contactos.java
@@ -0,0 +1,12 @@
+
+package TP_final;
+
+import javax.swing.JFrame;
+
+public class Contactos extends JFrame {
+    
+    public Contactos() {
+        setSize(500,500);   // establesco un tamaño al programa
+    }
+    
+}
---
+++ src/main/java/TP_final/Main.java
@@ -0,0 +1,10 @@
+
+package TP_final;
+
+public class Main {
+    public static void main(String[] args) {
+        Contactos agenda = new Contactos();     // creo una instancia de la clase contactos
+        
+        agenda.setVisible(true);
+    }
+}
