fc76fd08088357490ce7a742acf2c43928f75ace
louisrubin <miguel10.rbn@gmail.com>
2022-11-12 10:58:21 -0300

    Add new styles to the title Label
    agregué estilos a la etiqueta titulo, como cambio de color, fondo, tamaño y centrado

--- src/main/java/TP_final/Contactos.java
+++ src/main/java/TP_final/Contactos.java
@@ -2,13 +2,16 @@
 package TP_final;
 
 import java.awt.Color;
+import java.awt.Font;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Contactos extends JFrame {
     
     public Contactos() {
-        setSize(500,500);   //  un tamaño al programa
+        setSize(700,500);   //  un tamaño al programa
         setTitle("Agenda Trabajo Final");
         
         setResizable(false);    // no se puede agrandar el tamaño del programas
@@ -22,9 +25,28 @@ public class Contactos extends JFrame {
     private void EjecutarComponentes() {
         // creacion de un metodo para paneles y otros componentes
         JPanel panel = new JPanel();    // creacion del panel llamado "panel"
-        panel.setBackground(Color.green);
+        panel.setLayout(null);
+        
+        Font fuenteLetra = new Font("Serif", Font.BOLD, 25);
+        // jLabel1.setFont(jLabel1.getFont().deriveFont(Font.ITALIC));
+        
+        panel.setBackground(Color.DARK_GRAY);   // fondo gris oscuro
         this.getContentPane().add(panel);   // agrego el panel sobre la ventana
        
+        
+        JLabel titulo = new JLabel("AGENDA ELECTRÓNICA", SwingConstants.CENTER);
+        titulo.setBounds(0, 0, 700, 65);
+        titulo.setFont(fuenteLetra);
+        titulo.setForeground(Color.WHITE);
+        
+        titulo.setOpaque(true);
+        titulo.setBackground(Color.BLACK);
+        
+        
+        panel.add(titulo);
+       
+        
+        
     }
     
 }
